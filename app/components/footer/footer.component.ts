8322579c4033002c96732e0f0ec53e76f4e9bb18
kamleshKumarVerma <kaverma@redhat.com>
2016-05-12 17:11:38 +0530

    added footer
    Signed-off-by: kamleshKumarVerma <kaverma@redhat.com>

---
+++ app/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'footer',
+    templateUrl: '../prod/components/footer/footer.html',
+    styleUrls: ['../prod/components/footer/footer.css']
+})
+export class FooterComponent {
+	
+	constructor() {
+        
+    }
+
+}
+
+
